51e210b84adfdf8f92063292908482797fbd155b
snblack <koracell@yandex.ru>
2021-03-17 00:48:50 +0300

    add_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "poulpe_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'http://157.230.211.28/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
