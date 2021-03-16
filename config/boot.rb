13ab6cf726d3e49ac56ecfa6a241a45008e5b01c
snblack <koracell@yandex.ru>
2021-03-16 18:14:28 +0300

    return_boot

--- config/boot.rb
+++ config/boot.rb
@@ -2,11 +2,3 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-
-require 'bootsnap'
-Bootsnap.setup(
-  cache_dir:            'var/cache',          # Path to your cache
-  load_path_cache:      true,                 # Optimize the LOAD_PATH with a cache
-  compile_cache_iseq:   true,                 # Compile Ruby code into ISeq cache, breaks coverage reporting.
-  compile_cache_yaml:   true                  # Compile YAML into a cache
-)
