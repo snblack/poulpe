c9c35fadd317072126c7a64773feea728405e4d8
snblack <koracell@yandex.ru>
2021-03-17 01:07:45 +0300

    change_user_name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
     # user_name:            ENV['SMTP_USERNAME'],
     # password:             ENV['SMTP_PASSWORD'],
-    user_name:            'sender_poulpe@gmail.com',
+    user_name:            'sender_poulpe',
     password:             'Snblack06',
     authentication:       'plain',
     enable_starttls_auto: true }
