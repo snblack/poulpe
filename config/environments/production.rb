72b262c1faccafbaf76f826226706b655bf2ab2c
snblack <koracell@yandex.ru>
2021-03-17 01:14:11 +0300

    change_name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     port:                 587,
     # user_name:            ENV['SMTP_USERNAME'],
     # password:             ENV['SMTP_PASSWORD'],
-    user_name:            'sender_poulpe',
+    user_name:            'sender.poulpe@gmail.com',
     password:             'Snblack06',
     authentication:       'plain',
     enable_starttls_auto: true }
